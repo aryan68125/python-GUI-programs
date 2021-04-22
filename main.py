98ccb6500214ec84d2bba45a399ef5eddc29e543
Aditya Kumar <aryan68125@gmail.com>
2021-04-22 17:27:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from tkinter import *
 
 window = Tk()
+
+#creating widgets and GUI window components
 #generating window title
 window.title("My first python application")
 #creating a label widget
 myLabel1 = Label(window, text = "hello Aditya")
 myLabel2 = Label(window, text = "hello Aastha")
-# --putting it on the screen--
+
+# --putting widets and GUI window components on the screen--
 
 #grid system layout
 #it will layout the widgets in grid pattern with row and column
